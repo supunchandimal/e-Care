14e4610bfd1ec29b3af0524980e5214dbd7ec015
supunchandimal <48341172+supunchandimal@users.noreply.github.com>
2020-05-13 16:08:52 +0530

    login,registration and some other ui created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { FirstPageComponent } from './first-page/first-page.component';
+import { pathToFileURL } from 'url';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
+const routes: Routes = [
+  {path:'',redirectTo:'/home' ,pathMatch:'full'},
+  { path:'login', component: LoginComponentComponent },
+  {path:'home',component:FirstPageComponent},
+  {path:'joinnow', component:RegisterComponent},
+  {path:'contactus',component:ContactusComponent}
+  
+];
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { MainNavigationBarComponent } from './main-navigation-bar/main-navigation-bar.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstPageComponent,
+    MainFooterComponent,
+    MainNavigationBarComponent,
+    LoginComponentComponent,
+    RegisterComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
