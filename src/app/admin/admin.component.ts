9341a2a356a898fec748dad74cedfe781b54e066
hasandi_palliaguruge <pghasandi@gmail.com>
2020-05-28 10:02:40 +0530

    admin designed

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
+  // sidebar: any;
+isCollapsed : boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  // togglemenu(){
+  //   document.getElementById('sidebar').classList.toggle('active');
+  // }
+//   toggleClick() {
+//     this.sidebar.toggle();
+// }
+toggleCollapse(){
+  this.isCollapsed= !this.isCollapsed;
+}
 }
