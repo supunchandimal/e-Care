648c0a8dbd1ac5bd76bce87a394d785df7d9ab71
supunchandimal <48341172+supunchandimal@users.noreply.github.com>
2020-05-29 19:15:56 +0530

    ui and font changed ( 05/29)

---
+++ src/services/shared/contactmessages.ts
@@ -0,0 +1,12 @@
+export class Contactmessages {
+
+    constructor(
+        public firstname: string =null,
+        public secondname:string =null,
+        public email:string =null,
+        public phoneno:string =null,
+        public query:string=null
+    ){}
+
+    
+}
---
+++ src/services/shared/contactus.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Contactmessages } from './contactmessages';
+
+import { getSupportedInputTypes } from '@angular/cdk/platform';
+import { analytics, firestore } from 'firebase';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactusService {
+
+  message = new Contactmessages();   
+  succornot = Number;       
+  constructor( private db:AngularFirestore , private router:Router) { }
+ 
+  addmessage(message){
+   
+  
+    this.message= message;
+    
+    this.db.collection("contactus_messages").add({
+      firstname: message.firstname,
+      lastname: message.lastname,
+      email:message.email,
+      phoneno: message.phoneno,
+      subject:message.subject
+    }).then(()=>{
+      console.log("kljfsalkf");
+      this.router.navigate(['/contactus']);
+
+      
+    }).catch(function(error){
+      console.error("error" ,error);
+      
+      this.succornot=2;
+    })
+    
+}
+}
