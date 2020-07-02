20a0d7f6afd0c11d914a5bb4a9a1b328c1533184
hasandi_palliaguruge <pghasandi@gmail.com>
2020-07-02 13:25:42 +0530

    notification removed

--- src/app/manage-patient/patient-delete-dialog/patient-delete-dialog.component.ts
+++ src/app/manage-patient/patient-delete-dialog/patient-delete-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA,MatDialogRef} from  '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-patient-delete-dialog',
--- src/services/shared/patient-delete-dialog.service.ts
+++ src/services/shared/patient-delete-dialog.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { MatDialog, MatDialogConfig } from  '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
 import { PatientDeleteDialogComponent } from 'src/app/manage-patient/patient-delete-dialog/patient-delete-dialog.component';
 
 @Injectable({
