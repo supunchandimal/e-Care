12e6dc6fd6c1afdbe6ca4fe4aabf48b915c023e2
khguruge <gkasunharshana@gmail.com>
2020-10-12 01:39:19 +0530

    sidebar-updateAccount

--- src/app/doctor/modules/content-doc/content-doc.component.ts
+++ src/app/doctor/modules/content-doc/content-doc.component.ts
@@ -1,72 +1,17 @@
-import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content-doc',
   templateUrl: './content-doc.component.html',
   styleUrls: ['./content-doc.component.css']
 })
-export class ContentDocComponent implements OnChanges {
-  loggedIn : boolean;
-//  message : string;
 
 
-  constructor() {
-
-  }
-
-  ngOnChanges(changes : SimpleChanges): void {
-  //   console.log(changes);
-
-  //   const loggedIn = changes['loggedIn'];
-  //   if loggedInValue = 
-
-  // }
-
-}}
+export class ContentDocComponent implements OnInit {
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-content-doc',
-//   templateUrl: './content-doc.component.html',
-//   styleUrls: ['./content-doc.component.css']
-// })
-// export class ContentDocComponent implements OnInit {
-//   // today: number = Date.now();
-//   // minDate: Date;
-//   // maxDate: Date;
-//   // t07 = true;
-//   // indeterminate = false;
-//   // labelPosition: 'before' | 'after' = 'after';
-//   // disabled = false;
-
-//   constructor() { 
-//     // const currentYear = new Date().getFullYear();
-//     // this.minDate = new Date(this.today);
-//     // this.maxDate = new Date(currentYear + 1, 11, 31);
-//   }
-
-//   ngOnInit(): void {
-//   }
-
-//
+  ngOnInit(): void {
+    
+    }
+  
+}
